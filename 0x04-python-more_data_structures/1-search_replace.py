79480df11644d184d4ea230e55a63c6637861399
Markberyl <musangaberyl@gmail.com>
2022-09-29 01:16:27 -0700

    data structures

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,1 @@
+#!/usr/bin/python3def search_replace(my_list, search, replace): new_list = list(map(lambda x: replace if x == search else x, my_list)) return (new_list)
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,1 @@
+#!/usr/bin/python3def uniq_add(my_list=[]): sum = 0 my_list = list(set(my_list)) for i in my_list: sum += i return sum
